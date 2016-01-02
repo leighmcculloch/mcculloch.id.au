de81785b9756a2f2839b72eda03bd3cb97efd709
Leigh McCulloch <leigh@mcchouse.com>
2016-01-01 21:01:22 -0800

    Upgrade to middleman 4.

--- config.rb
+++ config.rb
@@ -1,11 +1,3 @@
-compass_config do |config|
-  config.output_style = :compact
-end
-
-configure :development do
-  activate :livereload
-end
-
 set :css_dir, 'stylesheets'
 
 configure :build do
@@ -16,7 +8,6 @@ configure :build do
   activate :relative_assets
 end
 
-# Sync with AWS S3
 activate :s3_sync do |s3_sync|
   s3_sync.bucket                     = 'mcchouse.com'
   s3_sync.region                     = 'us-east-1'
@@ -28,13 +19,4 @@ activate :s3_sync do |s3_sync|
   s3_sync.acl                        = 'public-read'
   s3_sync.encryption                 = false
   s3_sync.version_bucket             = false
-end
-
-activate :cdn do |cdn|
-  cdn.cloudflare = {
-    zone: 'mcchouse.com',
-    base_urls: ['http://mcchouse.com']
-  }
-  cdn.filter = /\.html$/
-  cdn.after_build = true
 end
